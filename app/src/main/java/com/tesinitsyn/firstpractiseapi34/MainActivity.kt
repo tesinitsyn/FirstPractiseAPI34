612f4bc4d0e23c880a0d858b92e066ee005d3087
tesinitsyn <sitiev174@gmail.com>
2023-09-08 13:39:37 +0300

    added nav host in activity_main.xml

--- app/src/main/java/com/tesinitsyn/firstpractiseapi34/MainActivity.kt
+++ app/src/main/java/com/tesinitsyn/firstpractiseapi34/MainActivity.kt
@@ -2,8 +2,12 @@ package com.tesinitsyn.firstpractiseapi34
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tesinitsyn.firstpractiseapi34.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var biniding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
